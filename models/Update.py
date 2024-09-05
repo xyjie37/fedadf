263bd268b1d5f43849c13ffb946532e24936f3c1
xyjie37 <131571777+xyjie37@users.noreply.github.com>
2024-09-05 13:35:43 +0800

    Update Update.py

--- models/Update.py
+++ models/Update.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Python version: 3.6
-
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -20,7 +16,6 @@ import numpy as np
 from sklearn.cluster import KMeans
 import warnings
 warnings.filterwarnings('ignore', category=FutureWarning)
-
 
 
 class DatasetSplit(Dataset):
@@ -401,14 +396,6 @@ class LocalUpdateFA(nn.Module):
         return net.state_dict(), sum(epoch_loss) / len(epoch_loss), agg_protos_label
 
 def classifier_calibration_loss(classifier_params, feature_anchors, num_classes):
-    """
-    计算分类器校准损失。
-    
-    :param classifier_params: 客户端的分类器参数 (phi_i)
-    :param feature_anchors: 特征锚点 (a_c)
-    :param num_classes: 类别总数 (C)
-    :return: 分类器校准损失
-    """
     logits = torch.matmul(classifier_params, feature_anchors.T)  # 计算分类器输出
     exp_logits = torch.exp(logits)  # 对输出进行指数运算
     log_prob = torch.log(exp_logits / torch.sum(exp_logits, dim=1, keepdim=True))  # 计算对数概率
@@ -417,15 +404,6 @@ def classifier_calibration_loss(classifier_params, feature_anchors, num_classes)
 
 class AnchorDistillationLoss(nn.Module):
     def __init__(self, student_outputs, teacher_outputs, anchors, temperature=1.0, device='cuda'):
-        """
-        初始化AnchorDistillationLoss模块。
-        参数:
-        - student_outputs: 学生模型输出的logits，维度 [batch_size, C]
-        - teacher_outputs: 教师模型输出的概率分布，维度 [batch_size, C]
-        - anchors: 特征锚点的集合，维度 [C, anchor_feature_dim]
-        - temperature: 温度参数，用于调整softmax分布的平滑程度
-        - device: 设备，默认为 'cuda'
-        """
         super(AnchorDistillationLoss, self).__init__()
         self.anchors = nn.Parameter(anchors, requires_grad=False)  # 锚点不应梯度下降
         self.temperature = temperature
@@ -564,176 +542,4 @@ class LocalUpdateFedAD(object):
         return net.state_dict(), sum(epoch_loss) / len(epoch_loss), agg_protos_label
 
 
-class LocalUpdateMLP(object):
-    def __init__(self, args, dataset=None, idxs=None, task=0, pretrain=False):
-        self.args = args
-        self.loss_func = nn.CrossEntropyLoss()
-        self.selected_clients = []
-        self.ldr_train = load_train_data(dataset, idxs, task, batch_size=self.args.local_bs)
-        self.pretrain = pretrain
-        self.prototypes = None  # 存储本地原型
-        self.num_classes = args.num_classes  
 
-    def update_prototypes(self, net, dataloader):
-        """
-        更新本地原型。
-        :param net: 模型的网络部分，用于提取特征
-        :param dataloader: 用于获取数据的Dataloader
-        """
-        net.eval()  # 切换到评估模式以提取特征
-        features = []
-        labels = []
-        with torch.no_grad():  # 禁用梯度计算
-            for images, label in dataloader:
-                images = images.to(self.args.device)
-                feature = net(images)  # 特征提取
-                features.append(feature.cpu())  # 收集特征
-                labels.append(label.cpu())
-
-        # 将数据转换为张量
-        features = torch.cat(features, dim=0)
-        labels = torch.cat(labels, dim=0)
-        
-        # 计算每个类别的原型
-        unique_labels = torch.unique(labels)
-        self.prototypes = torch.zeros(self.num_classes, features.size(1))  # 固定长度的原型列表
-        for i, lbl in enumerate(unique_labels):
-            idx = (labels == lbl).nonzero().view(-1)
-            if len(idx) > 0:
-                self.prototypes[lbl] = torch.mean(features[idx], dim=0)
-    def update_semantic_prototypes(self, net, dataloader, num_clusters):
-        """
-        使用k-means聚类算法更新语义原型。
-        :param net: 模型的网络部分，用于提取特征
-        :param dataloader: 用于获取数据的Dataloader
-        :param num_clusters: 要确定的语义原型数量
-        :return: 计算得到的语义原型张量
-        """
-        net.eval()  # 切换到评估模式以提取特征
-        features_all = []
-        labels_all = []
-        with torch.no_grad():  # 禁用梯度计算
-            for images, labels in dataloader:
-                images = images.to(self.args.device)
-                features = net(images)  # 特征提取
-                features_all.append(features.cpu())  # 收集特征
-                labels_all.append(labels.cpu())
-
-        # 将数据转换为numpy数组以进行聚类
-        features_all = np.concatenate(features_all, axis=0)
-        labels_all = np.concatenate(labels_all, axis=0)
-
-        # 使用k-means算法进行聚类
-        kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(features_all)
-
-        # 根据聚类结果初始化语义原型
-        semantic_prototypes = torch.zeros(num_clusters, features_all.shape[1])
-        for i in range(num_clusters):
-            # 选择属于当前聚类的所有样本
-            cluster_members = (kmeans.labels_ == i).nonzero()[0]
-            if len(cluster_members) > 0:
-                # 计算并更新语义原型
-                semantic_prototypes[i] = torch.from_numpy(np.mean(features_all[cluster_members], axis=0))
-
-        return semantic_prototypes
-    def prototype_regularization_loss(self, features, prototypes):
-        """
-        计算原型正则化损失。
-        
-        :param features: 模型输出的特征向量。
-        :param prototypes: 类原型。
-        :return: 原型正则化损失。
-        """
-        features = features.unsqueeze(1)  # [N, C] -> [N, 1, C]
-        prototypes = prototypes.unsqueeze(0)  # [C, D] -> [1, C, D]
-        dist = torch.norm(features - prototypes, dim=2)  # 计算特征和原型之间的距离
-        min_dist, _ = torch.min(dist, dim=1)  # 找到最近的原型 [N, 1]
-        reg_loss = torch.mean(min_dist)  # 计算原型正则化损失
-        return reg_loss
-    def semantic_prototype_regularization_loss(self, features, semantic_prototype):
-        """
-        计算语义原型正则化损失。
-
-        :param features: 模型输出的特征向量。
-        :param semantic_prototype: 语义原型。
-        :return: 语义原型正则化损失。
-        """
-        features = features.unsqueeze(1)  # [N, C] -> [N, 1, C]
-        semantic_prototype = semantic_prototype.unsqueeze(0)  # [C, D] -> [1, C, D]
-        dist = torch.norm(features - semantic_prototype, dim=2)  # 计算特征和语义原型之间的距离
-        min_dist, _ = torch.min(dist, dim=1)  # 找到最近的语义原型 [N, 1]
-        semantic_reg_loss = torch.mean(min_dist)  # 计算语义原型正则化损失
-        return semantic_reg_loss
-    def federated_inter_task_regularization_loss(self, old_prototypes, new_prototypes, alpha=0.5):
-        """
-        计算跨任务正则化损失。
-        
-        :param old_prototypes: 旧任务的原型。
-        :param new_prototypes: 新任务的原型。
-        :param alpha: 正则化系数。
-        :return: 跨任务正则化损失。
-        """
-        # 确保旧原型和新原型的尺寸一致
-        assert old_prototypes.size() == new_prototypes.size(), "Prototype sizes do not match!"
-        
-        # 计算原型之间的距离
-        dist = torch.norm(old_prototypes - new_prototypes, dim=1)
-        
-        # 使用KL散度作为损失函数
-        kl_divergence = torch.nn.functional.kl_div(
-            torch.log(1e-8 + torch.exp(dist)),
-            torch.zeros_like(dist), 
-            reduction='sum'
-        )
-        
-        # 返回加权的跨任务正则化损失
-        return alpha * kl_divergence / dist.size(0)
-
-    def train(self, net, lr, now_epoch, prototype, semantic_prototype, idx=-1, local_eps=None):
-        net.train()
-        optimizer = torch.optim.SGD(net.parameters(), lr=lr,
-                                    momentum=self.args.momentum,
-                                    weight_decay=self.args.wd)
-
-        epoch_loss = []
-        reg_loss_sum = 0
-        if local_eps is None:
-            local_eps = self.args.local_ep
-        self.update_prototypes(net, self.ldr_train)
-        for iter in range(local_eps):
-            batch_loss = []
-            for batch_idx, (images, labels) in enumerate(self.ldr_train):
-                images, labels = images.to(self.args.device), labels.to(self.args.device)
-                logits = net(images)
-                loss = self.loss_func(logits, labels)
-                if now_epoch > 0:
-                    # 计算原型正则化损失
-                    features = net(images)  # 重新获取特征，因为原型正则化需要
-                    reg_loss = self.prototype_regularization_loss(features, prototype)
-                    reg_loss_sum += reg_loss.item()  # 累积原型正则化损失
-                    semantic_reg_loss = self.semantic_prototype_regularization_loss(features, semantic_prototype)
-                    inter_task_reg_loss = self.federated_inter_task_regularization_loss(
-                        self.prototypes, 
-                        prototype
-                    )
-                    # 总损失是交叉熵损失和原型正则化损失的和
-                    total_loss = loss + 0.5 * reg_loss + 0.1 * semantic_reg_loss + 0.1 * inter_task_reg_loss
-                else:
-                    total_loss = loss
-                
-                # 更新模型参数
-                optimizer.zero_grad()
-                total_loss.backward()
-                optimizer.step()
-
-                # 累积批次损失
-                batch_loss.append(total_loss.item())
-
-            epoch_loss.append(sum(batch_loss)/len(batch_loss))
-
-        # 训练完成后更新原型和语义原型
-        #self.update_prototypes(net, self.ldr_train)
-        semantic_prototypes = self.update_semantic_prototypes(net, self.ldr_train, num_clusters=self.num_classes // 2) # 假设我们选择类别数量一半的聚类中心
-        self.semantic_prototypes = semantic_prototypes
-
-        return net.state_dict(), sum(epoch_loss) / len(epoch_loss), self.prototypes, self.semantic_prototypes
